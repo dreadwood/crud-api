39a4208e5161eabd4daea3d8d590c54146c63d63
srg <skabassjob@gmail.com>
2023-01-17 02:45:08 +0400

    feat: create router and connects it to the server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-import {printMessage} from './print';
+import http from 'node:http';
+import {defaultPort, serverListenMessage} from './const';
+import {router} from './router';
 
-printMessage('This is new Message!');
+const port = process.env.PORT ? Number(process.env.PORT) : defaultPort;
 
-console.log(process.env.PORT);
+const server = http.createServer(router);
+
+server.listen(port, () => {
+	console.info(`${serverListenMessage}: ${port}`);
+});
+
+//
+// server.on('error', ({message}) => {
+// 	console.error(`Ошибка: ${message}`);
+// });
+
+//
+// server.on('request', (req, res) => {
+// 	console.log('test');
+// 	console.log(req.host:);
+// });
 
